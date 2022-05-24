e6fab595bcdc82c839ec5a1c214dfff189957832
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-05-24 12:29:08 -0700

    add arg to getCategories, getClueHTML()

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 let categories = []
+const catNum = 5
+const offset = 30
 
 const gameBoard = document.getElementById("gameboard")
 
-async function getCategories() {
-    const response = await fetch(`https://jservice.io/api/categories?count=4`, {method: 'GET'})
+async function getCategories(num, offsetNum) {
+    const response = await fetch(`https://jservice.io/api/categories?count=${num}&offset=${offsetNum}`, {method: 'GET'})
     if(!response.ok){
         const message = `An error has occured: ${response.status}`
         throw new Error(message)
@@ -13,7 +15,7 @@ async function getCategories() {
     return categories
 }
 
-getCategories().then(categories =>{
+getCategories(catNum, offset).then(categories =>{
     console.log(categories)
     gameBoard.innerHTML = `${categories.map(getCategoryHTML).join('')}`
     
@@ -21,13 +23,17 @@ getCategories().then(categories =>{
 
 function getCategoryHTML(category){
    return `<div class="category-header">${category.title}</div>
-   <div class="clue row1">$100</div>
-   <div class="clue row2">$200</div>
-   <div class="clue row3">$300</div>
-   <div class="clue row4">$400</div>`  
+   ${getClueHTML(100)}
+   ${getClueHTML(200)}
+   ${getClueHTML(300)}
+   ${getClueHTML(400)}
+   `  
     }
 
 
-
+function getClueHTML(clueValue){
+    return `
+    <div class="clue row${clueValue/100}">$${clueValue}</div>`
+}
 
 
